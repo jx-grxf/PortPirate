5eeee67e0c23618a5ed201c046ef7bf8ff1bcb18
Johannes Grof <147989511+jx-grxf@users.noreply.github.com>
2026-05-17 12:39:10 +0200

    fix(updates): publish beta sparkle feed

--- Sources/MacDevCore/Services/UpdateService.swift
+++ Sources/MacDevCore/Services/UpdateService.swift
@@ -3,7 +3,8 @@ import Sparkle
 
 @MainActor
 public final class UpdateService: NSObject {
-  public static let appcastURLString = "https://github.com/jx-grxf/MacDev/releases/latest/download/appcast.xml"
+  public static let stableAppcastURLString = "https://github.com/jx-grxf/MacDev/releases/latest/download/appcast.xml"
+  public static let betaAppcastURLString = "https://github.com/jx-grxf/MacDev/releases/download/beta/appcast.xml"
 
   public static var isConfigured: Bool {
     guard let key = Bundle.main.object(forInfoDictionaryKey: "SUPublicEDKey") as? String else {
@@ -46,6 +47,11 @@ extension UpdateService: SPUUpdaterDelegate {
   }
 
   public func feedURLString(for updater: SPUUpdater) -> String? {
-    Self.appcastURLString
+    switch channelProvider() {
+    case .stable:
+      Self.stableAppcastURLString
+    case .beta:
+      Self.betaAppcastURLString
+    }
   }
 }
